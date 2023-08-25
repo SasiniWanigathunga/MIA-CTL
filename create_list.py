0951606ed47db47ef9eb5256db36dea22917b385
Sasini Wanigathunga <92870335+SasiniWanigathunga@users.noreply.github.com>
2023-08-25 05:59:08 +0530

    Update create_list.py

--- create_list.py
+++ create_list.py
@@ -156,8 +156,8 @@ def generate_folder(folder_num = 10,whole_file=r"./data_folder/supervised_folder
         file.close()
 
 if __name__ == '__main__':
-    generate_datatxt()
-    # split_ssl_and_sl()
+    # generate_datatxt()
+    split_ssl_and_sl()
     # generate_folder()
     pass
 
